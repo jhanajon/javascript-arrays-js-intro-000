d7cf5fa9b49ad26b8a33065bee58f35794a57c43
joe fisher <jhanajon@gmail.com>
2018-07-13 22:17:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,10 +6,10 @@ function addElementToBeginningOfArray(array, element) {
     return(array)
 }
 
-//addElementToBeginningOfArray(chocolateBars,'foo')
+addElementToBeginningOfArray(chocolateBars,'foo')
 
 
-destructivelyAddElementToBeginningOfArray(array, element){
+function destructivelyAddElementToBeginningOfArray(array, element){
     array = array.unshift(element)
     return(array)
 }
