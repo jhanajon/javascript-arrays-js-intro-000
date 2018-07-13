d5e2ae6e62408c0fa95b4e56e001fde61b6300fb
joe fisher <jhanajon@gmail.com>
2018-07-13 21:42:58 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,16 +1,7 @@
-//var cities = ["New York", "San Francisco"]
-// we can assign it to the existing `cities` variable
-// cities = ["Philadelphia", ...cities]
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 function addElementToBeginningOfArray(array, element) {
-    // chocolateBars.unshift('foo')
-
     array = [element, ...array]
-    // chocolateBars = `[$bars', ...$bars]`
     return(array)
 }
-//non destructive version
-//
 
-//     return(chocolateBars)
