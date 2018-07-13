66f3c41bbb346fad58003c289d608835fc1f4e44
joe fisher <jhanajon@gmail.com>
2018-07-13 22:28:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,3 +18,8 @@ function addElementToEndOfArray(array, element){
   array = [...array, element]
   return(array)
 }
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return(array)
+}
