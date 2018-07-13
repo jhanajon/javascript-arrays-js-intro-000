0c7fd34a0ad473a9c0cc42287f897bb346eed555
joe fisher <jhanajon@gmail.com>
2018-07-13 22:32:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,6 +24,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   return(array)
 }
 
-function accessElementInArray() {
-  
+function accessElementInArray(array, index) {
+  return array[3]
 }
