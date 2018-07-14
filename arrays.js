c922e076560069c4ffcd51ef968dbd655930a6e3
joe fisher <jhanajon@gmail.com>
2018-07-14 00:01:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -47,8 +47,8 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.slice(0, array.length - 1)
-  array = array.slice(0, array.length -1 )
+  array.pop()
+  array = array.pop()
   return(array)
 
 }
